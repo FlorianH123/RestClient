91e39984ec257236cbdc160e413f22f43220bc18
FlorianH123 <f.hennrich@gmx.de>
2017-12-19 12:18:34 +0100

    Impl

--- src/main/java/ClientApp.java
+++ src/main/java/ClientApp.java
@@ -4,11 +4,7 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
 public class ClientApp {
-    private static final String GET_PRIMES_AS_STRING_PATH = "getAsString";
-    private static final String GET_PRIMES_AS_ARRAY_PATH = "getAsArray";
-    private static final String GET_PRIMES_AS_OBJECT_PATH = "getAsObject";
     private static final String QUERY_PARAM      = "maxPrime";
-
     private WebTarget webTarget;
 
     public ClientApp(String baseUrl) {
@@ -17,21 +13,21 @@ public class ClientApp {
     }
 
     public String getPrimesAsString(int nPrimes) {
-        return webTarget.path(GET_PRIMES_AS_STRING_PATH)
+        return webTarget
                 .queryParam(QUERY_PARAM, nPrimes)
                 .request(MediaType.TEXT_PLAIN)
                 .get(String.class);
     }
 
     public long[] getPrimesAsArray(int nPrimes) {
-        return webTarget.path(GET_PRIMES_AS_ARRAY_PATH)
+        return webTarget
                 .queryParam(QUERY_PARAM, nPrimes)
                 .request(MediaType.APPLICATION_JSON)
                 .get(long[].class);
     }
 
     public Primes getPrimesAsObject(int nPrimes) {
-        return webTarget.path(GET_PRIMES_AS_OBJECT_PATH)
+        return webTarget
                 .queryParam(QUERY_PARAM, nPrimes)
                 .request(MediaType.APPLICATION_JSON)
                 .get(Primes.class);
